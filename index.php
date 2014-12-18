3aa6a2d165d40b049d25a7e8e81bfef146fdad24
Will Spiering <willspiering@gmail.com>
2014-12-17 19:53:29 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+View the Products
+*/
+
+
+$band = 'Three Days Grace';
+$album = 'One-X';
+$format = 'CD';
+$description = 'Good tunes';
+$price = 12.99;
+$qty = 10;
+
+
+//html
+echo
+"<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="UTF-8">
+  <title>Music Store</title>
+ </head>
+ <body>
+<h1>Music Store</h1>
+ </body>
+</html>";
+
+?>
